272cbcfd477a15b199924cd1a29603122be8dbdd
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-01-23 18:53:00 +0000

    Punch added in the current repository

---
+++ punch_config.py
@@ -0,0 +1,13 @@
+__config_version__ = 1
+
+GLOBALS = {
+    'serializer': '{{major}}.{{minor}}.{{patch}}',
+}
+
+FILES = ["setup.py"]
+
+VERSION = ['major', 'minor', 'patch']
+
+VCS = {
+    'name': 'git'
+}
