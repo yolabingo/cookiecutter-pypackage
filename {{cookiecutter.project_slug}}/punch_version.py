2e0cbbaea914d7d3e52d143c35f8d39aed12c6ef
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-02-01 16:16:06 +0000

    PEP8 fixed

--- {{cookiecutter.project_slug}}/punch_version.py
+++ {{cookiecutter.project_slug}}/punch_version.py
@@ -1,4 +1,4 @@
 {% set version_list = cookiecutter.version.split('.') %}
-major={{ version_list[0] }}
-minor={{ version_list[1] }}
-patch={{ version_list[2] }}
+major = {{ version_list[0] }}
+minor = {{ version_list[1] }}
+patch = {{ version_list[2] }}
